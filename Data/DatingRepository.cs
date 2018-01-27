0ec666077b144746cc28abb323a70bfc9b17e2a8
Alexis Gomez <gomez.alexis@gmail.com>
2018-01-27 17:59:38 -0600

    Retrieving Lists from Database

--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -25,21 +25,30 @@ namespace DatingApp.API.Data
             _context.Remove(entity);
         }
 
-        public Task<Photo> GetMainPhotoForUser(int userId)
+        public async Task<Like> GetLike(int userId, int recipientId)
         {
-            return _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
+            return await _context.Likes.FirstOrDefaultAsync(u => u.LikerId == userId && u.LikeeId == recipientId);
         }
 
-        public Task<Photo> GetPhoto(int id)
+        public async Task<Photo> GetMainPhotoForUser(int userId)
         {
-            var photo = _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
+            return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
+        }
+
+        public async Task<Photo> GetPhoto(int id)
+        {
+            var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
 
             return photo;
         }
 
         public async Task<User> GetUser(int id)
         {
-            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            var user = await _context.Users
+                .Include(l => l.Likee)
+                .Include(l => l.Liker)
+                .Include(p => p.Photos)
+                .FirstOrDefaultAsync(u => u.Id == id);
 
             return user;
         }
@@ -51,6 +60,19 @@ namespace DatingApp.API.Data
             users = users.Where(u => u.Id != userParams.UserId);
 
             users = users.Where(u => u.Gender == userParams.Gender);
+
+            if(userParams.Likers)
+            {
+                var userLikers = await GetUserLikers(userParams.UserId);
+                users = users.Where(u => userLikers.Any(liker => liker.LikerId == u.Id));
+            }
+            
+            if(userParams.Likees)
+            {
+                // filter the users into a single
+                var userLikees = await GetUserLikees(userParams.UserId);
+                users = users.Where(u => userLikees.Any(likee => likee.LikeeId == u.Id));
+            }
 
             if(userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
@@ -77,6 +99,20 @@ namespace DatingApp.API.Data
         public async Task<bool> SaveAll()
         {
             return await _context.SaveChangesAsync() > 0;
+        }
+
+        private async Task<IEnumerable<Like>> GetUserLikees(int id)
+        {
+            var user = await GetUser(id);
+            var userLikees = user.Liker.Where(u => u.LikerId == id).ToList();
+            return userLikees;
+        }
+
+        private async Task<IEnumerable<Like>> GetUserLikers(int id)
+        {
+            var user = await GetUser(id);
+            var userLikers = user.Likee.Where(u => u.LikeeId == id).ToList();
+            return userLikers;
         }
     }
 }
